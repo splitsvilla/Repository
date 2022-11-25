55ab80050da406ef84efd77b537d54e35a8dcab0
manishabhujbal <118868386+manishabhujbal@users.noreply.github.com>
2022-11-25 15:08:23 +0530

    Project structure created.
    Hi splitsvilla,
    I have assigned, start working on it.
    Regards,
    TL
    
    Co-Authored-By: splitsvilla <119038857+splitsvilla@users.noreply.github.com>

---
+++ arrayProgram/src/test/java/arrayProgram/arrayProgram/ArrayProg.java
@@ -0,0 +1,18 @@
+package arrayProgram.arrayProgram;
+
+public class ArrayProg {
+
+	public static void main(String[] args) {
+		
+		int arr[]= {1,2,3};
+		int s=0,avg;
+		for(int a:arr)
+		{
+			 s=s;
+		}
+		avg=s/arr.length;
+		System.out.println(avg);
+		
+	}
+	
+}
